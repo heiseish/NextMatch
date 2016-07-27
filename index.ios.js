23696c7bf784543598550d3af1b83c8d9e36be48
heiseish <dtrnggiang@gmail.com>
2016-07-27 10:42:04 +0800

    Add TabBar

--- index.ios.js
+++ index.ios.js
@@ -1,74 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-const Realm = require('realm');
+'use strict';
 
-// class NextMatch extends Component {
-//  render() {
-//    let realm = new Realm({
-//      schema: [{name: 'Dog', properties: {name: 'string'}}]
-//    });
+const {AppRegistry} = require('react-native');
+const setup = require('./setup/setup.js')
 
-//    realm.write(() => {
-//      realm.create('Dog', {name: 'Rex'});
-//    });
-
-//    return (
-//      <View style={styles.container}>
-//        <Text style={styles.welcome}>
-//          Count of Dogs in Realm: {realm.objects('Dog').length}
-//        </Text>
-//      </View>
-//    );
-//  }
-// }
-
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-class NextMatch extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Hi Boss Dao Truong Giang!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('NextMatch', () => NextMatch);
+AppRegistry.registerComponent('NextMatch',setup) 
+// () => NextMatch);
